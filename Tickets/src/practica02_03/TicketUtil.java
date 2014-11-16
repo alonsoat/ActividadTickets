86524735e7f33be3241f6618b39ecf59714afcf2
David <enylrad@gmail.com>
2014-11-16 18:31:33 +0100

    eny_003

--- Tickets/src/practica02_03/TicketUtil.java
+++ Tickets/src/practica02_03/TicketUtil.java
@@ -11,6 +11,9 @@ public class TicketUtil {
 		
 		/*
 		 * Consulta
+		 * Aquí recibira String busqueda, esto será el ID que sea un int ya lo controlo yo en interfaz,
+		 * Luego en activa yo te pasare el texto de Obert, Tancat o *
+		 * Y en departamento lo mismo 
 		 */
 		
 		
